3161c56fda78093a709f4eeaf4602ff88a313bfd
Evstafievskaya A.E <aee5@tpu.ru>
2021-06-17 11:02:45 +0700

    QR scanner without MVVM

--- app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.qrcodescanner
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -17,12 +22,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var codeScanner: CodeScanner
-    val CAMERA_PERMISSION_CODE = 1111
+    private val CAMERA_PERMISSION_CODE = 1111
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initCodeScanner()
+
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    "Scan result: ${it.text}", Toast.LENGTH_LONG
+                ).show()
+                openUrlInBrowser(it.text)
+            }
+        }
+
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    "Camera error: ${it.message}", Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        checkCameraPermission()
+
+        val reverseCameraImage: ImageView = reverseCameraImageView
+        reverseCameraImage.setOnClickListener {
+            reverseCamera()
+            Toast.makeText(
+                this,
+                "Reverse camera", Toast.LENGTH_LONG
+            ).show()
+        }
+
+        val cancelTextView: TextView = cancelTextView
+        cancelTextView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    fun initCodeScanner(){
         codeScanner = CodeScanner(this, scannerView)
         codeScanner.camera = CodeScanner.CAMERA_BACK
         codeScanner.formats = CodeScanner.ALL_FORMATS
@@ -30,28 +74,45 @@ class MainActivity : AppCompatActivity() {
         codeScanner.scanMode = ScanMode.SINGLE
         codeScanner.isAutoFocusEnabled = true
         codeScanner.isFlashEnabled = false
+    }
 
-        codeScanner.decodeCallback = DecodeCallback {
-            runOnUiThread {
-                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
-            }
-        }
-
-        codeScanner.errorCallback = ErrorCallback {
-            runOnUiThread {
-                Toast.makeText(this, "Camera error: ${it.message}", Toast.LENGTH_LONG).show()
-            }
-        }
-
-        checkCameraPermission()
+    fun reverseCamera() {
+        if (codeScanner.camera == CodeScanner.CAMERA_BACK)
+            codeScanner.camera = CodeScanner.CAMERA_FRONT
+        else codeScanner.camera = CodeScanner.CAMERA_BACK
     }
 
     fun checkCameraPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE
+            )
         } else {
             codeScanner.startPreview()
         }
+    }
+    private fun isValidUrl(url: String): Boolean {
+        return Patterns.WEB_URL.matcher(url).matches()
+    }
+
+    fun openUrlInBrowser(url: String) {
+        if (isValidUrl(url)) {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            if (browserIntent.resolveActivity(packageManager) != null)
+                startActivity(browserIntent)
+            else Toast.makeText(
+                this,
+                "Please, install a browser", Toast.LENGTH_LONG
+            ).show()
+        } else Toast.makeText(
+            this,
+            "The link is invalid", Toast.LENGTH_LONG
+        ).show()
     }
 
     override fun onRequestPermissionsResult(
@@ -59,10 +120,16 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode == CAMERA_PERMISSION_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
             codeScanner.startPreview()
         } else {
-            Toast.makeText(this, "Can not scan until you give the camera permission", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this,
+                "Can not scan until you give the camera permission", Toast.LENGTH_LONG
+            ).show()
         }
     }
 
